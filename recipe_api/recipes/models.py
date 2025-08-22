9e73ce72b4f300ade9179f6c8904eb0f427afe36
Skeehunxho <Kentmmmuia18@gmail.com>
2025-08-22 13:49:36 +0300

    Changes on models.py

--- recipe_api/recipes/models.py
+++ recipe_api/recipes/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    ingredients = models.TextField()  
+    instructions = models.TextField()
+    preparation_time = models.IntegerField(null=True, blank=True)
+    cooking_time = models.IntegerField(null=True, blank=True)
+    servings = models.IntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name="recipes")
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipes")
+
+    def __str__(self):
+        return self.title
