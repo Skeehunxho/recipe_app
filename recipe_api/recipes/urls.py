3ea6c7dbfb4d01dad1e999ad5c29f28e0fd6d932
Skeehunxho <Kentmmmuia18@gmail.com>
2025-08-25 15:08:15 +0300

    Update urls.py

--- recipe_api/recipes/urls.py
+++ recipe_api/recipes/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
-from .views import RecipeListCreateView, CategoryListView
+from .views import RecipeListCreateView, CategoryListView, register
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("recipes/", RecipeListCreateView.as_view(), name="recipe-list-create"),
     path("categories/", CategoryListView.as_view(), name="category-list"),
-]
-from .views import register
-from rest_framework.authtoken.views import obtain_auth_token
-
-urlpatterns = [
-    path('register/', register, name='register'),
-    path('login/', obtain_auth_token, name='login'),
+    path("register/", register, name="register"),
+    path("login/", obtain_auth_token, name="login"),
 ]
