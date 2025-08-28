64b9e034249f84fd5550dc7f68f70a30a9a60bf4
Skeehunxho <Kentmmmuia18@gmail.com>
2025-08-28 11:15:29 +0300

    Update urls.py

--- recipe_api/recipe_api/urls.py
+++ recipe_api/recipe_api/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('recipes.urls')),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')), # to enable the browsable API login/logout
 ]
