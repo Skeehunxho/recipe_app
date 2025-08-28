e0b99d7c8c507458e7cca2aab343141bb9ec46aa
Skeehunxho <Kentmmmuia18@gmail.com>
2025-08-28 10:20:22 +0300

    Added and API root

--- recipe_api/recipes/urls.py
+++ recipe_api/recipes/urls.py
@@ -1,7 +1,23 @@
 from django.urls import path
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from .views import RegisterView, CustomAuthToken, RecipeListCreateView, CategoryListView
 
+
+# API root
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'recipes': reverse('recipe-list', request=request, format=format),
+        'categories': reverse('category-list', request=request, format=format),
+        'register': reverse('register', request=request, format=format),
+        'login': reverse('login', request=request, format=format),
+    })
+
+
 urlpatterns = [
+    path("", api_root, name="api-root"),  
     path("recipes/", RecipeListCreateView.as_view(), name="recipe-list"),
     path("categories/", CategoryListView.as_view(), name="category-list"),
     path("register/", RegisterView.as_view(), name="register"),
