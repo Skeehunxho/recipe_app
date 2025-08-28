c479386bfce97a848e1791155ba699d22954f711
Skeehunxho <Kentmmmuia18@gmail.com>
2025-08-28 09:19:41 +0300

    Add a RegisterSerializer

--- recipe_api/recipes/serializers.py
+++ recipe_api/recipes/serializers.py
@@ -18,3 +18,17 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = "__all__"
+        
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "password"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            password=validated_data["password"]
+        )
+        return user
